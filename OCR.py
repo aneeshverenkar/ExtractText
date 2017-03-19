66c93ee4725322934676c465d55be161d60677d7
aneeshverenkar <aneesh.verenkar@gmail.com>
2017-03-19 02:07:29 -0500

    yay

--- OCR.py
+++ OCR.py
@@ -1,11 +1,17 @@
 import pytesseract
 from PIL import Image, ImageEnhance, ImageFilter
 
-im = Image.open("/home/aneesh/Documents/testOCR2.png") # the second one
-im = im.filter(ImageFilter.MedianFilter())
-enhancer = ImageEnhance.Contrast(im)
-im = enhancer.enhance(2)
-im = im.convert('1')
-im.save('/home/aneesh/SFHacks/temp2.jpg')
-text = pytesseract.image_to_string(Image.open('/home/aneesh/SFHacks/temp2.jpg'))
-print(text)
+def extractText(image):
+  im = image
+  im = im.filter(ImageFilter.MedianFilter())
+  enhancer = ImageEnhance.Contrast(im)
+  im = enhancer.enhance(2)
+  im = im.convert('1')
+  im.save('temp2.jpg')  # image from which you want to extract text
+  text = pytesseract.image_to_string(Image.open('temp2.jpg'))
+  return text
+
+img = Image.open("/dir/image.png")  # input image
+print(extractText(img))
+
+
